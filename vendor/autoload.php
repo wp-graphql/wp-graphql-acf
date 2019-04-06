630c5de88cd0e1066f60f634998c8ce7199848f0
Ben Allfree <ben@benallfree.com>
2019-04-06 09:17:29 -0700

    phpcs tooling

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit43d505e613353a4948867689bb69bf5b::getLoader();
+return ComposerAutoloaderInit78b1922b8102c4b4d425a5476ce4478e::getLoader();
--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -7,4 +7,5 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'WPGraphQL\\ACF\\' => array($baseDir . '/src'),
+    'Dealerdirect\\Composer\\Plugin\\Installers\\PHPCodeSniffer\\' => array($vendorDir . '/dealerdirect/phpcodesniffer-composer-installer/src'),
 );

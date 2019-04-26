b5bc39da2f09619c894b1bfcd4a6c0bf86823c58
Jason Bahl <jasonbahl@mac.com>
2019-04-25 21:23:41 -0600

    - Update version number

--- wp-graphql-acf.php
+++ wp-graphql-acf.php
@@ -7,7 +7,7 @@
  * Author URI:      https://www.wpgraphql.com
  * Text Domain:     wp-graphql-acf
  * Domain Path:     /languages
- * Version:         0.1.1
+ * Version:         0.1.2
  *
  * @package         WPGraphQL_ACF
  */

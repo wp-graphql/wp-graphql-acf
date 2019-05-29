ce0482f24a6c77222010556cb96657733ad4c2a2
Jason Bahl <jasonbahl@mac.com>
2019-05-29 16:09:52 -0600

    - Bump version to v0.1.5 - Fix bug with local JSON not working (fields aren't explicitly set to active in local json) - Fix bug with empty flex fields throwing an exception

--- wp-graphql-acf.php
+++ wp-graphql-acf.php
@@ -7,7 +7,7 @@
  * Author URI:      https://www.wpgraphql.com
  * Text Domain:     wp-graphql-acf
  * Domain Path:     /languages
- * Version:         0.1.4
+ * Version:         0.1.5
  *
  * @package         WPGraphQL_ACF
  */

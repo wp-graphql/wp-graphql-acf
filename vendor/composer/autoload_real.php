b82216960be1a46d2eb35a37ea8619e4ace87b6b
Jason Bahl <jasonbahl@mac.com>
2019-08-12 17:22:41 -0600

    - remove dev dependencies

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -47,24 +47,6 @@ class ComposerAutoloaderInit3fbb9843644ca7389b188c6353f475fc
 
         $loader->register(true);
 
-        if ($useStaticLoader) {
-            $includeFiles = Composer\Autoload\ComposerStaticInit3fbb9843644ca7389b188c6353f475fc::$files;
-        } else {
-            $includeFiles = require __DIR__ . '/autoload_files.php';
-        }
-        foreach ($includeFiles as $fileIdentifier => $file) {
-            composerRequire3fbb9843644ca7389b188c6353f475fc($fileIdentifier, $file);
-        }
-
         return $loader;
-    }
-}
-
-function composerRequire3fbb9843644ca7389b188c6353f475fc($fileIdentifier, $file)
-{
-    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        require $file;
-
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }

6b2b0fb6d6bd7ed3bb334c996277956dc2e57d97
Jason Bahl <jasonbahl@mac.com>
2020-10-22 08:45:59 -0600

    - update version for release

--- wp-graphql-acf.php
+++ wp-graphql-acf.php
@@ -7,7 +7,7 @@
  * Author URI:        https://www.wpgraphql.com
  * Text Domain:       wp-graphql-acf
  * Domain Path:       /languages
- * Version:           0.3.6
+ * Version:           0.4.0
  * Requires PHP:      7.0
  * GitHub Plugin URI: https://github.com/afragen/github-updater
  *

cf6f37c8ff97486bac9bbe9a68ac47d9b1f33cf2
Mark Kelnar <markkelnar@gmail.com>
2021-03-31 08:36:01 -0500

    Download tagged release version of wp-graphql zip

--- docker/app.entrypoint.sh
+++ docker/app.entrypoint.sh
@@ -42,7 +42,7 @@ fi
 # Install and activate WPGraphQL
 if [ ! -f "${PLUGINS_DIR}/wp-graphql/wp-graphql.php" ]; then
     wp plugin install \
-        https://github.com/wp-graphql/wp-graphql/archive/${CORE_BRANCH-master}.zip \
+        https://github.com/wp-graphql/wp-graphql/releases/download/${CORE_VERSION-v1.3.3}/wp-graphql.zip \
         --activate --allow-root
 else
     wp plugin activate wp-graphql --allow-root

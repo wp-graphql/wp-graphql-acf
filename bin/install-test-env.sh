d8795da16227a2c81abaef7ec5711ddad3a8881a
Jason Bahl <jasonbahl@mac.com>
2021-07-19 13:55:12 -0600

    - update install-test-env.sh

--- bin/install-test-env.sh
+++ bin/install-test-env.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-if [[ ! -f ".env" ]]; then
+if [ ! -f .env ]; then
   echo "No .env file was detected. .env.dist has been copied to .env"
   echo "Open the .env file and enter values to match your local environment"
-  cp .env.dist .env
+  cp ./.env.dist ./.env
+  export $(cat .env | xargs)
 fi
 
-source .env.dist
+source .env
 
 env | sort
 
